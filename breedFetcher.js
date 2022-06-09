ea7ac9f0829f3d4cd8db29c9d9bec66637fed298
AVacAdok <avakuang8@gmail.com>
2022-06-09 23:34:13 +0000

    refactor code

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,10 +1,8 @@
 const request = require("request");
 
-const breed = process.argv.slice(2);
+const fetchBreedDescription = function(breedName) {
 
-const breedInfo = function(breed) {
-
-  request(`https://api.thecatapi.com/v1/breeds/search?q=${breed}`, (error, response, body) => {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
     if (error) {
       // Print the error if one occurred
@@ -18,21 +16,13 @@ const breedInfo = function(breed) {
     } else {
       console.log(`Fail to find breed`);
     }
-    // if (response !== 200) {
-    //   console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // }
-    
-    // if(data[0].description) {
-    //   console.log('body:', body); // Print the HTML for the Google homepage.
-    // }
-
-    // console.log(data)
-    // console.log(data[0].description);
-    // console.log(typeof data);
-    // //Access the first entry in the data array and print out the description for the user.
-    // console.log(data[0].description)
 
   });
 };
 
-breedInfo(breed);
+
+module.exports = {
+
+  fetchBreedDescription
+};
+
